477dd2c2abe662a12e4d375c6840e238edbdce3d
rodolfo-cacacho <rodolfo.cacachoestrada@study.thws.de>
2024-12-05 22:06:21 +0100

    Changing model to Multi-E5

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ EMBEDDING_MODELS = {
                                    "api_usage":False,
                                    "instruction":"Given a query, retrieve relevant information from the available documents"}}
 
-EMBEDDING_MODEL = "intfloat/multilingual-e5-large-instruct"
+EMBEDDING_MODEL = "jinaai/jina-embeddings-v3"
 EMBEDDING_MODEL_NAME = EMBEDDING_MODEL.split("/")[1].replace('_','-').lower()
 EMBEDDING_MODEL_DIM = EMBEDDING_MODELS[EMBEDDING_MODEL]["dimension"]
 EMBEDDING_MODEL_API = EMBEDDING_MODELS[EMBEDDING_MODEL]["api_usage"]
