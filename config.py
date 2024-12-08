6255b39dc88886d26460ac80b3e7bc863636adc6
rodolfo-cacacho <rodolfo.cacachoestrada@study.thws.de>
2024-12-08 19:02:16 +0100

    Multi 250

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ EMBEDDING_MODELS = {
                                    "instruction":"Given a query, retrieve relevant information from the available documents",
                                    "normalize":True}}
 
-EMBEDDING_MODEL = "jinaai/jina-embeddings-v2-base-de"
+EMBEDDING_MODEL = "intfloat/multilingual-e5-large-instruct"
 EMBEDDING_MODEL_NAME = EMBEDDING_MODEL.split("/")[1].replace('_','-').lower()
 EMBEDDING_MODEL_DIM = EMBEDDING_MODELS[EMBEDDING_MODEL]["dimension"]
 EMBEDDING_MODEL_API = EMBEDDING_MODELS[EMBEDDING_MODEL]["api_usage"]
