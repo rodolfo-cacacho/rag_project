ec45db9a2cbbcd37e38d5c70475aac7cd04ff47c
rodolfo-cacacho <rodolfo.cacachoestrada@study.thws.de>
2024-12-08 21:08:50 +0100

    Normal Params

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # RAG CONFIGURATIONS
 
-MAX_TOKENS = 250
+MAX_TOKENS = 500
 SUFFIX = 'clean'
 
 EMBEDDING_MODELS = {
@@ -29,7 +29,7 @@ EMBEDDING_MODELS = {
                                    "instruction":"Given a query, retrieve relevant information from the available documents",
                                    "normalize":True}}
 
-EMBEDDING_MODEL = "jinaai/jina-embeddings-v2-base-de"
+EMBEDDING_MODEL = "jinaai/jina-embeddings-v3"
 EMBEDDING_MODEL_NAME = EMBEDDING_MODEL.split("/")[1].replace('_','-').lower()
 EMBEDDING_MODEL_DIM = EMBEDDING_MODELS[EMBEDDING_MODEL]["dimension"]
 EMBEDDING_MODEL_API = EMBEDDING_MODELS[EMBEDDING_MODEL]["api_usage"]
