83b132b3d0b837190fdbbf43ec5d8767c822cebc
rodolfo-cacacho <rodolfo.cacachoestrada@study.thws.de>
2024-12-09 14:39:12 +0100

    changing model

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ EMBEDDING_MODELS = {
                                    "instruction":"Given a query, retrieve relevant information from the available documents",
                                    "normalize":True}}
 
-EMBEDDING_MODEL = "aari1995/German_Semantic_V3"
+EMBEDDING_MODEL = "jinaai/jina-embeddings-v3"
 EMBEDDING_MODEL_NAME = EMBEDDING_MODEL.split("/")[1].replace('_','-').lower()
 EMBEDDING_MODEL_DIM = EMBEDDING_MODELS[EMBEDDING_MODEL]["dimension"]
 EMBEDDING_MODEL_API = EMBEDDING_MODELS[EMBEDDING_MODEL]["api_usage"]
@@ -44,7 +44,7 @@ DIST_THRESHOLD = 0.2
 
 MX_RESULTS_QUERY = 25
 
-ALPHA_VALUE = 0.9
+ALPHA_VALUE = 0.1
 
 GEN_PROMPTS = 3
 
